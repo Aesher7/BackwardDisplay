6d8f76f729198925191d669ef11858d45cec1593
Aesher7 <93729335+Aesher7@users.noreply.github.com>
2024-01-17 09:36:34 -0700

    Create ArrayBackwardDisplayTest.java

---
+++ ArrayBackwardDisplayTest.java
@@ -0,0 +1,9 @@
+public class ArrayBackwardDisplayTest {
+    public static void main(String[] args) {
+        int[] array = {777, 4, 96, 330, 19};
+
+        System.out.println("Displaying portion of array backward:");
+        ArrayBackwardDisplay.displayArrayBackward(array, 0, 3);
+    }
+}
+
